578051141562785b865f3389dc4b4085e8395757
moosakhan1 <144359037+moosakhan1@users.noreply.github.com>
2023-11-08 21:59:25 +0500

    practrice set

---
+++ app.js
@@ -0,0 +1,41 @@
+// let a ={
+//     name:"Moosa",
+//     math:99,
+//     urdu:99,
+//     sst:99,
+//     english:99,
+//     computer:99,
+// }
+// console.log(a)
+// for (let b in a){
+//        console.log(b,a[b])
+// }
+// let obj ={
+//         harry:98,
+//         rohan:70,
+//         aakash:7,
+// }
+// for (let a in obj){
+//     console.log(a,obj[a])
+// }
+
+// let correctNumber = 2;
+// let a =prompt("enter your a number");
+// if(a === correctNumber){
+//     console.log("try again band kardo")
+// }
+// else{
+//     console.log("try again")
+// }
+
+// function mean (a,b,c,d){
+//     return a+b+c+d/4;
+// }
+// let s = mean(99.9,3,3,2);
+// console.log(s)
+function mul (a,b,c,d) {
+    return a+b+c+d && a/b/c/d && a*b*c*d;
+}
+
+let v = mul(2,1,1,1)
+console.log(v)
